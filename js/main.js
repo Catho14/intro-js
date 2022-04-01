4eb2423977a9662f8b6bed42e52cbba8a4acb38e
Carlos Cathi <98982846+Catho14@users.noreply.github.com>
2022-03-31 21:46:59 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/* alert("Hola Mundo!!!");
+ 
+console.log("Este es otro mensaje");
+*/
+var nombre="Carlos Cathi";
+let edad = 10;
+// edad = "No quiero decirlo ";
+let titulo = document.getElementById('titulo');
+let mensaje = "Mi nombre es "+ nombre + " y mi edad es " + (edad+20);
+
+console.log(mensaje);
+
+titulo.innerHTML = mensaje;
+titulo.style.color = '#0000ee';
+//Poner en mayusculas
+titulo.style.textTransform= 'uppercase';
+
+titulo.onclick = function(){
+    var mensaje2= "Le diste clic";
+    titulo.innerHTML = "Le diste clic";
+    titulo.innerHTML = mensaje2.toUpperCase()
+    mensaje2.toLowerCase()
+};
